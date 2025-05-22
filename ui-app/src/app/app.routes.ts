e496aab6f836420668558e3cc8a71a3e2806706d
Cristian777-ai <63517418+Cristian777-ai@users.noreply.github.com>
2025-05-22 00:35:12 -0500

    Update app.routes.ts

--- ui-app/src/app/app.routes.ts
+++ ui-app/src/app/app.routes.ts
@@ -1,16 +1,13 @@
-
 import { Routes } from '@angular/router';
-import { AuthGuard } from './services/auth.guard';
-import { UserListComponent } from './users/user-list/user-list.component';
-import { LoginComponent }  from './auth/login/login.component';
+import { LoginComponent }          from './auth/login/login.component';
+import { UserListComponent }       from './users/user-list/user-list.component';
+import { UserDetailComponent }     from './users/user-detail/user-detail.component';
+import { AuthGuard }               from './services/auth.guard';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  {
-    path: 'users',
-    component: UserListComponent,
-    canActivate: [AuthGuard]
-  },
-  { path: '', redirectTo: 'users', pathMatch: 'full' },
-  { path: '**', redirectTo: 'users' }
+  { path: 'login',    component: LoginComponent },
+  { path: 'users',    component: UserListComponent, canActivate: [AuthGuard] },
+  { path: 'users/:id',component: UserDetailComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' }
 ];
