34ad100323590d107a7911ff0905d1b245c01e0c
Cristian777-ai <63517418+Cristian777-ai@users.noreply.github.com>
2025-05-21 23:53:55 -0500

    Update auth.guard.ts

--- ui-app/src/app/services/auth.guard.ts
+++ ui-app/src/app/services/auth.guard.ts
@@ -1,34 +1,21 @@
-
-import { Injectable }                 from '@angular/core';
-import {
-  CanActivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  Router,
-  UrlTree
-}                                      from '@angular/router';
-import { AuthService }                from './auth.service';
-import { Observable }                 from 'rxjs';
-import { map, take }                  from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-  constructor(
-    private auth: AuthService,
-    private router: Router
-  ) {}
+  constructor(private auth: AuthService, private router: Router) {}
 
- 
-  canActivate(
-    _route?: ActivatedRouteSnapshot,
-    _state?: RouterStateSnapshot
-  ): Observable<boolean | UrlTree> {
+  canActivate() {
     return this.auth.isLoggedIn$.pipe(
-      take(1),
-      map(logged => logged
-        ? true
-        : this.router.parseUrl('/login')
-      )
+      map(isLogged => {
+        if (!isLogged) {
+          this.router.navigate(['/login']);
+          return false;
+        }
+        return true;
+      })
     );
   }
 }
