cdda1bb31aa8419d6ca926c5b16af4ba5b92af1e
Cristian777-ai <63517418+Cristian777-ai@users.noreply.github.com>
2025-05-21 22:38:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const path    = require('path');
 const app     = express();
 
-app.use(express.static(path.join(__dirname, 'ui-app/dist/ui-app')));
+app.use(express.static(path.join(__dirname, 'ui-app')));
 
 app.get('/*', (_, res) =>
-  res.sendFile(path.join(__dirname, 'ui-app/dist/ui-app/index.html'))
+  res.sendFile(path.join(__dirname, 'ui-app/src/index.html'))
 );
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+ui-app/src/index.html
